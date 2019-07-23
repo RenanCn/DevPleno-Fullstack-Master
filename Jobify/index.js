0bc3341df637ec6bbf553c8ed79f49f5ee9cd33e
RenanCn <renan.rcn@hotmail.com>
2019-07-23 14:28:39 -0300

    atualização

--- Jobify/index.js
+++ Jobify/index.js
@@ -10,7 +10,14 @@ app.use(express.static('public'))
 
 app.get('/', async(request, response) => {
     const db = await dbConnection
-    const categorias = await db.all('select * from categorias;')
+    const categoriasDb = await db.all('select * from categorias;')
+    const vagas = await db.all('select * from vagas;')
+    const categorias = categoriasDb.map(cat =>{
+        return{
+            ...cat,
+            vagas: vagas.filter( vaga => vaga.categoria === cat.id )
+        }
+    })
     response.render('home', {
         categorias
     })
@@ -25,8 +32,12 @@ app.get('/vaga', (request, response) => {
 const init = async() =>{
     const db = await dbConnection
     await db.run('create table if not exists categorias (id INTEGER PRIMARY KEY, categoria TEXT)')
-    const categoria = 'Time de Engenharia'
-    await db.run(`insert into categorias(categoria) values('${categoria}') `)
+    await db.run('create table if not exists vagas (id INTEGER PRIMARY KEY, categoria INTEGER, titulo TEXT, descricao TEXT)')
+    //const categoria = 'Time de Marketing'
+    //await db.run(`insert into categorias(categoria) values('${categoria}') `)
+    //const vaga = 'Marketing Digital'
+    //const descricao = 'Vaga para marketing digital.'
+    //await db.run(`insert into vagas(categoria, titulo, descricao) values(2, '${vaga}', '${descricao}') `)
 
 }
 
